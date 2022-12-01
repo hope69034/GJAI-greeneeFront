fbd4aa96ce3e0e6671de16499d4c4589a0dfd3fd
sehyoungchoi <hope69034@gmail.com>
2022-12-01 09:14:42 +0900

    1r

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,17 +1,15 @@
 import React from "react";
  /*  import AddEvent from "../components/AddEvent"; */
-import TalkChat from '../components/chatbot/TalkChat'
+import TalkChat from '../components/chatbot/api/TalkChat'
 import ChatbotSteps from '../components/chatbot/ChatbotSteps'  
-import Talkbot from '../components/chatbot/Talkbot' 
-import Talk from '../components/chatbot/Talk'  
+
 function ChatPage() {
   
   return (
     <>
   <TalkChat /> 
   <ChatbotSteps></ChatbotSteps>   
-  <Talkbot></Talkbot>
-  <Talk></Talk>
+
     </>
   );
 };

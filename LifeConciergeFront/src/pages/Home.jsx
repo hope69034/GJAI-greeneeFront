5aedd460471b55be7d32e4d0247eba4c7ebc4ce7
sehyoungchoi <hope69034@gmail.com>
2022-12-01 18:12:15 +0900

    backPlusFront

---
+++ LifeConciergeFront/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import AddEventButton from "../components/FreqCompo/AddEventButton";
+import Timeline from "../components/Timeline";
+import HeaderAlarm from "../components/HeaderAlarm";
+import LabelBottomNavigation from "../components/LabelBottomNavigation";
+export default () => {
+  return (
+    <>
+      <HeaderAlarm />
+      <Timeline></Timeline>
+      <AddEventButton />
+      <LabelBottomNavigation></LabelBottomNavigation>
+    
+    </>
+  );
+};
+

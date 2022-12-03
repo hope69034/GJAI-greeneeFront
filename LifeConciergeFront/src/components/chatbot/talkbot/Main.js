5a153fef247622b7f28137197dd691b2a2681564
sehyoungchoi <hope69034@gmail.com>
2022-12-03 20:31:23 +0900

    332

--- LifeConciergeFront/src/components/chatbot/talkbot/Main.js
+++ LifeConciergeFront/src/components/chatbot/talkbot/Main.js
@@ -1,41 +1,31 @@
-import React from "react"; 
-import {Toast,ToastContainer} from 'react-bootstrap'
-import Footer from './Footer'
+import React from "react";
+import { Toast, ToastContainer } from "react-bootstrap";
 import a2 from "../../../img/greenee.png";
 function Main(props) {
   return (
-      <main className = "main-container">
-          
-          <ToastContainer bsPrefix = "toast-main-container">
-            {
-                props.message.map((item) => (
-                    item.from ? (
-                        /* bg:버블컬러 */
-                        <Toast     bg = 'warning' className = "my-message">
-
-                            <Toast.Body>{item.msg}</Toast.Body>
-                        </Toast>
-                    )
-                    : (
-                        
-                     
- 
-
-                        <Toast     className = "other-message">
-                          
-                     
-                            <Toast.Body><img
-                        src={a2}
-                        className={"a2"}
-                        style={{ width: "70px", padding: "20px"}}
-                      />{item.msg}</Toast.Body>
-                        </Toast>
-                    )
-                ))
-                
-            }
-                  </ToastContainer>
-      </main>
+    <main className="main-container">
+      <ToastContainer bsPrefix="toast-main-container">
+        {props.message.map((item) =>
+          item.from ? (
+            /* bg:버블컬러 */
+            <Toast bg="warning" className="my-message">
+              <Toast.Body>{item.msg}</Toast.Body>
+            </Toast>
+          ) : (
+            <Toast className="other-message">
+              <Toast.Body>
+                <img
+                  src={a2}
+                  className={"a2"}
+                  style={{ width: "10px", padding: "20px" }}
+                />
+                {item.msg}
+              </Toast.Body>
+            </Toast>
+          )
+        )}
+      </ToastContainer>
+    </main>
   );
 }
 
